c2430f4c56f13d777bf560f9e445a3353393fe31
Administrator <Administrator@MSDN-SPECIAL>
2017-12-29 22:00:04 +0900

    NEW_20171229: 스프링 junit 테스트 환경 만들기

--- src/test/java/com/setvect/nowhappy/test/MainTestBase.java
+++ src/test/java/com/setvect/nowhappy/test/MainTestBase.java
@@ -3,15 +3,19 @@ package com.setvect.nowhappy.test;
 import javax.transaction.Transactional;
 
 import org.junit.runner.RunWith;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring/context-common.xml", "classpath:spring/context-hibernate.xml",
-		"classpath:springmvc/context-*.xml" })
+import com.setvect.nowhappy.NowHappyApplication;
+
+/**
+ * 스프링 테스트
+ */
+@RunWith(SpringRunner.class)
 @Transactional
-@Rollback(true)
+@SpringBootTest(classes = { NowHappyApplication.class })
+@TestPropertySource(locations = "file:" + NowHappyApplication.CONFIG_CONFIG_PROPERTIES_TEST)
 public class MainTestBase {
 
 }
